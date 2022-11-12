5872b7bded50466df99d58bd481d2d729fcbf301
Paulo Henrique <paulohenso@hotmail.com>
2022-11-12 14:50:58 -0300

    configurado emailservice

---
+++ Chronos.CrossCutting/DependencyInjection/ConfigureServices.cs
@@ -0,0 +1,15 @@
+﻿using Chronos.Domain.Interfaces.Services;
+using Chronos.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Chronos.CrossCutting.DependencyInjection
+{
+    public static class ConfigureServices
+    {
+        public static void ConfigureDependenciesService(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<IEmailService, EmailService>();            
+        }
+
+    }
+}
---
+++ Chronos.Domain/Settings/EmailSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Chronos.Domain.Settings
+{
+    public class EmailSettings
+    {
+        public string ServerSmtp { get; set; }
+        public int ServerPortSmtp { get; set; }
+        public string UserSmtp { get; set; }
+        public string Password { get; set; }
+        public string MailSender { get; set; }
+    }
+}
