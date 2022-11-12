aa5c5c8f377f6c1507b4c436465415fa8e4886c2
rafael <@Rafael_gs>
2022-11-12 10:00:24 -0300

    Subir Contracts

---
+++ Chronos.Domain/Contracts/Request/LoginRequest.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Chronos.Domain.Contracts.Request
+{
+    public class LoginRequest
+    {
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+}
---
+++ Chronos.Domain/Contracts/Request/TarefaRequest.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Chronos.Domain.Contracts.Request
+{
+    public class TarefaRequest
+    {
+        public string Descricao { get; set; }
+        public int ProjetoId { get; set; }
+    }
+}
