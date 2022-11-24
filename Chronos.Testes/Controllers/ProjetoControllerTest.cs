6d83f030b85568ac70295e8fa25a775c18050e27
rafael <@Rafael_gs>
2022-11-24 11:49:51 -0300

    Teste Projeto Controller

--- Chronos.Testes/Controllers/ProjetoControllerTest.cs
+++ Chronos.Testes/Controllers/ProjetoControllerTest.cs
@@ -1,117 +1,174 @@
-﻿//using Chronos.Api.Controllers;
-//using Chronos.Domain.Contracts.Request;
-//using Chronos.Domain.Contracts.Response;
-//using Chronos.Domain.Interfaces.Services;
-//using Chronos.Testes.Settings;
-//using Microsoft.AspNetCore.Mvc;
-//using System.Security.Cryptography.X509Certificates;
+﻿using Chronos.Api.Controllers;
+using Chronos.Domain.Contracts.Request;
+using Chronos.Domain.Contracts.Response;
+using Chronos.Domain.Interfaces.Services;
+using Chronos.Testes.Settings;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Security.Cryptography.X509Certificates;
 
-//namespace Chronos.Testes.Controllers
-//{
-//    [TestClass]
-//    public class ProjetoControllerTest
-//    {
-//        private readonly Mock<IProjetoService> _mockProjetoService;
-//        private readonly Fixture _fixture;
+namespace Chronos.Testes.Controllers
+{
+    [TestClass]
+    public class ProjetoControllerTest
+    {
+        private readonly Mock<IProjetoService> _mockProjetoService;
+        private readonly Fixture _fixture;
 
-//        public ProjetoControllerTest()
-//        {
-//            _mockProjetoService = new Mock<IProjetoService>();
-//            _fixture = FixtureConfig.Get();
-//        }
+        public ProjetoControllerTest()
+        {
+            _mockProjetoService = new Mock<IProjetoService>();
+            _fixture = FixtureConfig.Get();
+        }
 
-//        [TestMethod]
-//        public async Task TestCadastrarAsync()
-//        {
-//            var message = _fixture.Create<MensagemResponse>();
-//            var request = _fixture.Create<ProjetoRequest>();
-//            _mockProjetoService.Setup(mock => mock.CadastrarAsync(request)).ReturnsAsync(message);
+        [TestMethod]
+        public async Task TestObterPorUsuarioIdAsync()
+        {
+            var response = _fixture.Create<UsuarioResponse>();
+            var projetosResponse = _fixture.Create<List<ProjetoResponse>>();
+            _mockProjetoService.Setup(mock => mock.ObterPorUsuarioId(response.Id)).ReturnsAsync(projetosResponse);
 
-//            var controller = new Projeto(_mockProjetoService.Object);
+            var controller = new ProjetoController(_mockProjetoService.Object);
 
-//            var result = await controller.CadastrarAsync(request);
+            var result = await controller.ObterPorUsuarioIdAsync(response.Id);
 
-//            Assert.IsNotNull(result);
+            Assert.IsNotNull(result);
 
-//            var createdResult = result as CreatedResult;
-//            Assert.IsNotNull(createdResult);
-//        }
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            var value = okResult.Value as List<ProjetoResponse>;
+            Assert.IsNotNull(value);
+            Assert.AreEqual(projetosResponse.Count, value.Count);
+        }
 
-//        [TestMethod]
-//        public async Task TestAlterarAsync()
-//        {
-//            var message = _fixture.Create<MensagemResponse>();
-//            var request = _fixture.Create<ProjetoRequest>();
-//            _mockProjetoService.Setup(mock => mock.AlterarAsync(It.IsAny<int>(), request)).ReturnsAsync(message);
+        [TestMethod]
+        public async Task TestInativarColaboradoresAsync()
+        {
+            var message = _fixture.Create<MensagemResponse>();
+            var projetoResponse = _fixture.Create<ProjetoResponse>();
+            var request = _fixture.Create<ColaboradoresRequest>();
+            _mockProjetoService.Setup(mock => mock.InativarColaboradores(projetoResponse.Id, request)).ReturnsAsync(message);
 
-//            var controller = new UsuarioController(_mockProjetoService.Object);
+            var controller = new ProjetoController(_mockProjetoService.Object);
 
-//            var result = await controller.AlterarAsync(It.IsAny<int>(), request);
+            var result = await controller.InativarColaboradoresAsync(projetoResponse.Id, request);
 
-//            Assert.IsNotNull(result);
+            Assert.IsNotNull(result);
 
-//            var okResult = result as OkObjectResult;
-//            Assert.IsNotNull(okResult);
-//        }
+            var createdResult = result as OkObjectResult;
+            Assert.IsNotNull(createdResult);
+        }
 
-//        [TestMethod]
-//        public async Task TestDeletarAsync()
-//        {
-//            var message = _fixture.Create<MensagemResponse>();
-//            var request = _fixture.Create<ProjetoRequest>();
-//            _mockProjetoService
-//                .Setup(mock => mock.DeletarAsync(It.IsAny<int>()))
-//                .ReturnsAsync(message);
+        [TestMethod]
+        public async Task TestAdicionarColaboradoresAsync()
+        {
+            var message = _fixture.Create<MensagemResponse>();
+            var projetoResponse = _fixture.Create<ProjetoResponse>();
+            var request = _fixture.Create<ColaboradoresRequest>();
+            _mockProjetoService.Setup(mock => mock.AdicionarColaboradores(projetoResponse.Id, request)).ReturnsAsync(message);
 
-//            var controller = new UsuarioController(_mockProjetoService.Object);
+            var controller = new ProjetoController(_mockProjetoService.Object);
 
-//            var result = await controller.DeletarAsync(It.IsAny<int>());
+            var result = await controller.AdicionarColaboradoresAsync(projetoResponse.Id, request);
 
-//            Assert.IsNotNull(result);
+            Assert.IsNotNull(result);
 
-//            var okResult = result as OkObjectResult;
-//            Assert.IsNotNull(okResult);
-//        }
+            var createdResult = result as CreatedResult;
+            Assert.IsNotNull(createdResult);
+        }
 
-//        [TestMethod]
-//        public async Task TestObterTodosAsync()
-//        {
-//            var response = _fixture.Create<List<UsuarioResponse>>();
-//            _mockProjetoService.Setup(mock => mock.ObterTodosAsync()).ReturnsAsync(response);
+        [TestMethod]
+        public async Task TestCadastrarAsync()
+        {
+            var message = _fixture.Create<MensagemResponse>();
+            var request = _fixture.Create<ProjetoRequest>();
+            _mockProjetoService.Setup(mock => mock.CadastrarAsync(request)).ReturnsAsync(message);
 
-//            var controller = new UsuarioController(_mockProjetoService.Object);
+            var controller = new ProjetoController(_mockProjetoService.Object);
 
-//            var result = await controller.ObterTodosAsync();
+            var result = await controller.CadastrarAsync(request);
 
-//            Assert.IsNotNull(result);
+            Assert.IsNotNull(result);
 
-//            var okResult = result as OkObjectResult;
-//            Assert.IsNotNull(okResult);
-//            var value = okResult.Value as List<UsuarioResponse>;
-//            Assert.IsNotNull(value);
-//            Assert.AreEqual(response.Count, value.Count);
-//        }
+            var createdResult = result as CreatedResult;
+            Assert.IsNotNull(createdResult);
+        }
 
-//        [TestMethod]
-//        public async Task TestObterPorIdAsync()
-//        {
-//            var response = _fixture.Create<UsuarioResponse>();
-//            _mockProjetoService
-//                .Setup(mock => mock.ObterPorIdAsync(response.Id))
-//                .ReturnsAsync(response);
+        [TestMethod]
+        public async Task TestAlterarAsync()
+        {
+            var message = _fixture.Create<MensagemResponse>();
+            var request = _fixture.Create<ProjetoRequest>();
+            _mockProjetoService.Setup(mock => mock.AlterarAsync(It.IsAny<int>(), request)).ReturnsAsync(message);
 
-//            var controller = new UsuarioController(_mockProjetoService.Object);
+            var controller = new ProjetoController(_mockProjetoService.Object);
 
-//            var result = await controller.ObterPorIdAsync(response.Id);
+            var result = await controller.AlterarAsync(It.IsAny<int>(), request);
 
-//            Assert.IsNotNull(result);
+            Assert.IsNotNull(result);
 
-//            var okResult = result as OkObjectResult;
-//            Assert.IsNotNull(okResult);
-//            var value = okResult.Value as UsuarioResponse;
-//            Assert.IsNotNull(value);
-//            Assert.AreEqual(response.Id, value.Id);
-//        }
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+        }
 
-//    }
-//}
+        [TestMethod]
+        public async Task TestDeletarAsync()
+        {
+            var message = _fixture.Create<MensagemResponse>();
+            var request = _fixture.Create<ProjetoRequest>();
+            _mockProjetoService
+                .Setup(mock => mock.DeletarAsync(It.IsAny<int>()))
+                .ReturnsAsync(message);
+
+            var controller = new ProjetoController(_mockProjetoService.Object);
+
+            var result = await controller.DeletarAsync(It.IsAny<int>());
+
+            Assert.IsNotNull(result);
+
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+        }
+
+        [TestMethod]
+        public async Task TestObterTodosAsync()
+        {
+            var response = _fixture.Create<List<ProjetoResponse>>();
+            _mockProjetoService.Setup(mock => mock.ObterTodosAsync()).ReturnsAsync(response);
+
+            var controller = new ProjetoController(_mockProjetoService.Object);
+
+            var result = await controller.ObterTodosAsync();
+
+            Assert.IsNotNull(result);
+
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            var value = okResult.Value as List<ProjetoResponse>;
+            Assert.IsNotNull(value);
+            Assert.AreEqual(response.Count, value.Count);
+        }
+
+        [TestMethod]
+        public async Task TestObterPorIdAsync()
+        {
+            var response = _fixture.Create<ProjetoResponse>();
+            _mockProjetoService
+                .Setup(mock => mock.ObterPorIdAsync(response.Id))
+                .ReturnsAsync(response);
+
+            var controller = new ProjetoController(_mockProjetoService.Object);
+
+            var result = await controller.ObterPorIdAsync(response.Id);
+
+            Assert.IsNotNull(result);
+
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            var value = okResult.Value as ProjetoResponse;
+            Assert.IsNotNull(value);
+            Assert.AreEqual(response.Id, value.Id);
+        }
+
+    }
+}
